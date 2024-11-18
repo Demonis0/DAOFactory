f6d96cae623249ed72bb7650c8f59440f8495e6c
Demonis0 <demonisgames@gmail.com>
2024-11-18 23:34:40 +0100

    MissingAnnotationException

---
+++ src/main/java/com/demonis/daofactory/errors/MissingAnnotationException.java
@@ -0,0 +1,7 @@
+﻿package com.demonis.daofactory.errors;
+
+public class MissingAnnotationException extends RuntimeException {
+    public MissingAnnotationException(String message) {
+        super(message);
+    }
+}
